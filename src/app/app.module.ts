87eda3334fec74e6fcb7a46bba36c89e29fa8dc6
taharbenoudjit <taharbenoudjit@gmail.com>
2020-05-05 01:12:08 +0200

    display mongodb server and set angular routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseCreateComponent } from './components/course-create/course-create.component';
+import { CourseEditComponent } from './components/course-edit/course-edit.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseCreateComponent,
+    CourseEditComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
